4a284dc54063abae67a18ade435b8deb49ec7454
Princess Ann <113954505+GeguillanAnn@users.noreply.github.com>
2022-11-18 09:21:42 +0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,39 @@
+#Ordering System#
+
+print("\n" * 5)
+
+import datetime
+import cs
+
+list_icecream = []
+list_drinks = []
+list_service = []
+
+list_item_prize = [0] * 100
+
+var_discount_1 = 200
+var_discount_2 = 1500
+var_discount_3 = 6000
+var_discount_1_rate = 0.5
+var_discount_2_rate = 0.10
+var_dicount_3_rate = 0.20
+
+navigator_symbo; = "/"
+if os.name == "nt":
+    navigator_symbol = "\\"
+
+def def_defult () :
+    global list_drinks, list_icecream, list_service, list_item_order, list_item_price
+    list_item_order = [0] * 100
+def_default ()
+
+def def_main():
+    while True:
+        print("==" + 20 + "ICE CREAM PARLOR CASHIER" + "==" + 24 + "\n" )
+        print("==" + 31 + "MAIN MENU" + "==" + 32 + "\n"
+             "\t(O) ORDER\n"
+             "\t(R) REPORT\n"
+             "\t(P) PAYMENT\n"
+             "\t(E) EXIT\n" +
+            "_" + 72)
+        
